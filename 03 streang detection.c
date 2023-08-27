63dc864614c60dccb8e00f54c96379315cad9640
MuksituilIslam <muksitul15-3962@diu.edu.bd>
2023-08-27 22:48:30 +0600

    using condition

--- 03 streang detection.c
+++ 03 streang detection.c
@@ -3,26 +3,32 @@
 
 int main()
 {
-    int c=0, i;
+    int c=0, i, j;
     char st[100];
     printf("Enter Sting : ");
     gets(st);
     int len=strlen(st);
-    printf("String : %d", len);
+    //printf("String : %d", len);
 
     for(i=0; i<len; i++)
     {
-        if(st[i]=='*' && st[i+1]=='/')
+        if((st[i]=='\'' && st[i-1]!='\\') || (st[i]=='"' && st[i-1]!='\''))
         {
-            printf("\nStirng found");
-            c++;
+            for(j=i+1; j<len; j++)
+            {
+                if((st[i]=='\'' && st[i+1]!='\\') || (st[i]=='"' && st[i+1]!='\''))
+                {
+                    printf("Stirng found : %c",st[i]);
+                    c++;
+                    break;
+                }
+            } 
         }
     }
     if(c==0)
     {
-        printf("\nThere is no string");
+        printf("There is no string");
     }
-
     return 0;
 
 }
